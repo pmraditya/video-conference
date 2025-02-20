336f8a0e793632ec30c0ad3bfa61e473eb9ad8ac
pmraditya <pmradityaair1@gmail.com>
2025-02-20 21:56:36 +0530

    added spring boot project

--- TestingClass.java
+++
@@ -1,5 +0,0 @@
-public class TestingClass {
-    public static void main(String[] args) {
-        System.out.println("everything working fine");
-    }
-}
---
+++ src/main/java/com/example/videoconference/VideoConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoConferenceApplication.class, args);
+    }
+
+}
